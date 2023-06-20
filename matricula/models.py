bb4ceb28c6fe6fc75e358258cc30c4896e63a62d
XaviFibla <xavi.fibla@gmail.com>
2023-06-20 19:15:43 +0200

    create models Pla Estudis

--- matricula/models.py
+++ matricula/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class PlaEstudi(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Modul(models.Model):
+    studie = models.ForeignKey(PlaEstudi, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+    
+class UnitatFormativa(models.Model):
+    module = models.ForeignKey(Modul, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+    hours = models.IntegerField(default=0)
